01bd5a4a89d019e7b8d28a6cdaf4b97d93eee68a
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-02-02 19:11:14 -0300

    comentários extras para a função init gpio

--- OneShot.c
+++ OneShot.c
@@ -12,24 +12,27 @@
 
 // Função para inicializar as GPIOs
 void init_gpio() {
+    // Leds
     gpio_init(LED_GREEN);
     gpio_init(LED_BLUE);
     gpio_init(LED_RED);
+    // Botão
     gpio_init(BUTTON_A);
 
+    // Direção dos pinos dos LEDs
     gpio_set_dir(LED_GREEN, GPIO_OUT);
     gpio_set_dir(LED_BLUE, GPIO_OUT);
     gpio_set_dir(LED_RED, GPIO_OUT);
+    // Direção do pino do botão
     gpio_set_dir(BUTTON_A, GPIO_IN);
 
+    // Configura o resistor de pull-up para o botão
     gpio_pull_up(BUTTON_A);
 }
 
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     return 0;
 }
-
-
 
 int main() {
     stdio_init_all();
